4699c0ac8d4071e8000fc4dcc780fdd8565cfda0
Rahul kumar <82836340+Rahul-kumar200@users.noreply.github.com>
2023-03-25 21:08:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('Getting request')
+    res.send('Getting request from front')
 })
 
 app.get('/signin',(req,res)=>{
